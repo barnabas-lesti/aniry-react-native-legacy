b327cba4e5866ed276d7b9d1b3fb3bcebeb278e3
Barnabas Lesti <barnabas.lesti@gmail.com>
2022-09-18 09:30:21 +0200

    Removed color updates.

--- src/app/theme/appTheme.ts
+++ src/app/theme/appTheme.ts
@@ -4,12 +4,6 @@ export const appTheme = {
   ...MD3LightTheme,
   roundness: 2,
   version: 3,
-  colors: {
-    ...MD3LightTheme.colors,
-    primary: '#3498db',
-    secondary: '#f1c40f',
-    tertiary: '#a1b2c3',
-  },
   gaps: {
     small: 10,
     medium: 20,
