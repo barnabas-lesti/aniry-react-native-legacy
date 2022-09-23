d4201cfe5084b9f11737dec0a4ef340a5a60f303
Barnabas Lesti <barnabas.lesti@gmail.com>
2022-09-23 08:17:13 +0200

    Updated editing screens.

--- src/features/ingredient/components/IngredientEditor.tsx
+++ src/features/ingredient/components/IngredientEditor.tsx
@@ -133,7 +133,7 @@ export function IngredientEditor(props: IngredientEditorProps) {
   }
 
   return (
-    <View style={style}>
+    <View style={[styles.container, style]}>
       <AppButtonGroup
         style={styles.buttonGroup}
         buttons={[
@@ -228,6 +228,9 @@ export function IngredientEditor(props: IngredientEditorProps) {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   buttonGroup: {
     marginBottom: appTheme.gaps.medium,
   },
--- src/features/ingredient/screens/IngredientEditScreen.tsx
+++ src/features/ingredient/screens/IngredientEditScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 import { AppStackScreenProps } from 'app/models';
 import { appTheme } from 'app/theme';
@@ -20,20 +20,18 @@ export function IngredientEditScreen(props: IngredientEditScreenProps) {
   } = props;
 
   return (
-    <View style={styles.container}>
-      <IngredientEditor
-        ingredient={ingredient}
-        onDiscard={() => navigation.goBack()}
-        onAfterSave={() => navigation.push('IngredientHome')}
-        onAfterDelete={() => navigation.push('IngredientHome')}
-      />
-    </View>
+    <IngredientEditor
+      style={styles.container}
+      ingredient={ingredient}
+      onDiscard={() => navigation.goBack()}
+      onAfterSave={() => navigation.push('IngredientHome')}
+      onAfterDelete={() => navigation.push('IngredientHome')}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     padding: appTheme.gaps.medium,
-    flex: 1,
   },
 });
